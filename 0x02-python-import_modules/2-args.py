48351d1942a404a00c584d53cad2d2e481d1ab5a
Joseph <josephgreat2003@gmail.com>
2022-09-22 11:44:29 -0700

    args2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-import sys
 
-argv = sys.argv
-if len(argv) > 2:
-    print("{:d} {:s}".format(len(argv) - 1, "argument"))
-else:
-    print("{:d} {:s}".format(len(argv) - 1, "arguments"))
+if __name__ == "__main__":
+    import sys
 
-for i in range(1, len(argv)):
-    print("{:d}: {:s}".format(i, argv[i]))
+    argv = sys.argv
+    if len(argv) > 2:
+        print("{:d} {:s}".format(len(argv) - 1, "argument"))
+    else:
+        print("{:d} {:s}".format(len(argv) - 1, "arguments"))
+
+    for i in range(1, len(argv)):
+        print("{:d}: {:s}".format(i, argv[i]))
