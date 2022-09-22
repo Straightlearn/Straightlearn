e9ad252d521bad31494af1a218556a8ed4829ea7
Joseph <josephgreat2003@gmail.com>
2022-09-22 11:47:02 -0700

    typo error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,10 +4,10 @@ if __name__ == "__main__":
     import sys
 
     argv = sys.argv
-    if len(argv) > 2:
-        print("{:d} {:s}".format(len(argv) - 1, "argument"))
+    if len(argv) <= 2:
+        print("{:d} {:s}:".format(len(argv) - 1, "argument"))
     else:
-        print("{:d} {:s}".format(len(argv) - 1, "arguments"))
+        print("{:d} {:s}:".format(len(argv) - 1, "arguments"))
 
     for i in range(1, len(argv)):
         print("{:d}: {:s}".format(i, argv[i]))
