9f97e0f7208f6a777b12292f4065013623181587
Joseph <josephgreat2003@gmail.com>
2022-09-22 14:55:31 -0700

    refactored code

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -11,16 +11,15 @@ if __name__ == "__main__":
     b = int(argv[3])
     operator = argv[2]
 
-    if operator != "+" and operator != "-" and operator != "\
-            *" and operator != "/":
+    if operator == "+":
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    elif operator == "-":
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    elif operator == "*":
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    elif operator == "/":
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
 
-    if operator == "+":
-        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    if operator == "-":
-        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    if operator == "*":
-        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    if operator == "/":
-        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
