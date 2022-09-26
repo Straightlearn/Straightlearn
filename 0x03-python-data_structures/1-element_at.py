6896ca2d54643ffcce7488ec52afe19bfa327a64
Joseph <josephgreat2003@gmail.com>
2022-09-26 14:31:31 -0700

    Retrieve element using loop

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,4 +2,6 @@
 def element_at(my_list, idx):
     if 0 > idx > len(my_list):
         return None
-    return my_list[idx]
+    for i in my_list:
+        if i == idx:
+            return my_list[i]
