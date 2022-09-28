4a12997e3921486f4c24cc70e0fcac921e770744
Joseph <josephgreat2003@gmail.com>
2022-09-28 08:36:23 -0700

    converted integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -10,10 +10,10 @@ def roman_to_int(roman_string):
 
     for numeral in roman_string:
         if numeral not in roman_int:
-            return 0
+            continue
         if (prev_numeral and roman_int[prev_numeral] < roman_int[numeral]):
             integer += roman_int[numeral] - roman_int[prev_numeral] * 2
         else:
             integer += roman_int[numeral]
         prev_numeral = numeral
-    return integer
+    return int(integer)
