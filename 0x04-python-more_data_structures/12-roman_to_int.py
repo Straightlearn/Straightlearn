51ce3e095eba97618aff4005042593162257b883
Joseph <josephgreat2003@gmail.com>
2022-09-28 07:51:33 -0700

    roman to int

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    integer = 0
+    roman_int = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    prev_numeral = ''
+    for numeral in roman_string:
+        if prev_numeral and roman_int[prev_numeral] < roman_int[numeral]:
+                integer -= roman_int[numeral]
+        else:
+            integer += roman_int[numeral]
+        prev_numeral = numeral
+    return abs(integer)
