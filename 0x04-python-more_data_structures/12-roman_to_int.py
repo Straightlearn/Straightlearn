41a5489820dd77320f67365569bf7f5417af7b3c
Joseph <josephgreat2003@gmail.com>
2022-09-28 08:18:56 -0700

    changed logic

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,13 +4,15 @@ def roman_to_int(roman_string):
     roman_int = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
                  'D': 500, 'M': 1000}
     prev_numeral = ''
+    
     if not roman_string:
         return integer
+
     for numeral in roman_string:
         if not roman_int[numeral]:
             return 0
         if (prev_numeral and roman_int[prev_numeral] < roman_int[numeral]):
-            integer -= roman_int[numeral]
+            integer += roman_int[numeral] - roman_int[prev_numeral] * 2
         else:
             integer += roman_int[numeral]
         prev_numeral = numeral
