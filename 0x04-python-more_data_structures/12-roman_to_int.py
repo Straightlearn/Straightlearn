3d189ff8e69a1016f76571b2a5e296db014ae30e
Joseph <josephgreat2003@gmail.com>
2022-09-28 08:24:45 -0700

    code style check

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,7 @@ def roman_to_int(roman_string):
     roman_int = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
                  'D': 500, 'M': 1000}
     prev_numeral = ''
-    
+
     if not roman_string:
         return integer
 
@@ -16,4 +16,4 @@ def roman_to_int(roman_string):
         else:
             integer += roman_int[numeral]
         prev_numeral = numeral
-    return abs(integer)
+    return integer
