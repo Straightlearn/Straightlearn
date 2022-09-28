494c5aaa1947a200a5a74fd3ff033c6ec7afd695
Joseph <josephgreat2003@gmail.com>
2022-09-28 06:01:32 -0700

    made search-replace dynamic

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,8 +2,8 @@
 def search_replace(my_list, search, replace):
     new_list = []
     for i in my_list:
-        if i == 2:
-            new_list.append(89)
+        if i == search:
+            new_list.append(replace)
             continue;
         new_list.append(i)
     return new_list
