050abac69a3b84906d891fb2dc87fb280736bb0e
Joseph <josephgreat2003@gmail.com>
2022-10-10 14:59:30 -0700

    added element print statement

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,8 +4,9 @@ def safe_print_list(my_list=[], x=0):
 
     for i in range(x):
         try:
-            my_list[i]
+            print(my_list[i], end="")
         except IndexError:
             break
         nb_print += 1
+    print()
     return nb_print
