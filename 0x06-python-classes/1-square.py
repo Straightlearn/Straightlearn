c9aec84315d9e0102b6e4e05f5b37fe0adab28f7
Joseph <josephgreat2003@gmail.com>
2022-10-11 16:23:00 -0700

    added comments

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 class Square:
+    """Square class with initialization
+    Attributes:
+        size: private attribute
+    """
     def __init__(self, size):
+        """Iinitializing Square instance
+        Args:
+            size: number of size of square
+        """
         self.__size = size
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 class Square:
+    """Square class with __init__"""
     def __init__(self, size=0):
+        """initialize an instance of the Square class and validate the size arg
+        Args:
+            size (int): size of the square
+        """
         if type(size) == int:
             if size < 0:
                 raise ValueError("size must be >= 0")
