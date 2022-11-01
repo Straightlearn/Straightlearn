4fbbfdc6632a41d42224a6fd3778a2c0fbadb006
josephgreat <josephgreat2003@gmail.com>
2022-10-31 21:54:27 -0700

    created models folder

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Base module"""
+
+
+import unittest
+class Base:
+    """A Class for our test"""
+    _nb_objects = 0
+
+    def __init__(self, id=None):
+        """initialize Base for instances"""
+
+        if (id is not Non):
+            self.id = id
+        else:
+            __nb_objects = __nb_objects + 1
+            self.id = __nb_objects
+
+if __name__ == "__main__":
+    unittest.main()
