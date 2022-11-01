646827a794f8237910116adb0026e62cb69ecdd1
josephgreat <josephgreat2003@gmail.com>
2022-10-31 22:13:50 -0700

    checked code

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -3,18 +3,21 @@
 
 
 import unittest
-class Base:
+
+
+class Base():
     """A Class for our test"""
-    _nb_objects = 0
+    __nb_objects = 0
 
     def __init__(self, id=None):
         """initialize Base for instances"""
 
-        if (id is not Non):
+        if (id is not None):
             self.id = id
         else:
-            __nb_objects = __nb_objects + 1
-            self.id = __nb_objects
+            Base.__nb_objects = Base.__nb_objects + 1
+            self.id = Base.__nb_objects
+
 
 if __name__ == "__main__":
     unittest.main()
