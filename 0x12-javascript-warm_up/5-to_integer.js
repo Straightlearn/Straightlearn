a7bfda97e9c3ad248595ad7828202ab295fe1a63
josephgreat <josephgreat2003@gmail.com>
2022-12-20 08:23:51 -0800

    check word length

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 // pints My number: <first argument converted in integer>
 
-let number = process.argv[2];
-if (isNaN(number)) {
+if (isNaN(process.argv[2])) {
 	console.log('Not a number');
 } else {
-	console.log('My number: ' + parseInt(number));
+	console.log('My number: ' + parseInt(process.argv[2]));
 }
