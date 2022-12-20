ee1d52e7653a87b40429dc78b34894edc9119139
josephgreat <josephgreat2003@gmail.com>
2022-12-20 06:39:17 -0800

    executable

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,5 +2,8 @@
 // Prints My number: <first argument converted in integer>
 
 let number = process.argv[2];
-if (isNaN(number)) console.log('Not a number');
-else console.log('My number: ' + parseInt(number));
+if (isNaN(number)) {
+	console.log('Not a number');
+} else {
+	console.log('My number: ' + parseInt(number));
+}
