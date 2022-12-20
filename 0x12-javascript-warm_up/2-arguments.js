3f3e515e3a450046c62dadf4fa3bf61a5e6b3787
josephgreat <josephgreat2003@gmail.com>
2022-12-20 05:31:58 -0800

    made adjustmenst based on standard

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
+
 const { argv } = require('node:process');
 
-if (argv[3]) console.log("Arguments found");
-else if (argv[2]) console.log("Argument found");
-else console.log("No argument");
+if (argv.length === 2) console.log('No argument');
+else if (argv.length === 3) console.log('Argument found');
+else console.log('Arguments found');
