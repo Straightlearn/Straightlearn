897f752bcb7e7ddfda00bc30e1607cffddc593b2
josephgreat <josephgreat2003@gmail.com>
2022-12-20 05:41:30 -0800

    reformatted code

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
+//Prints a message depending on the number of arguments
 
-const { argv } = require('node:process');
 let message = 'Arguments found';
-if (argv.length === 2) message = 'No argument';
-else if (argv.length === 3) message = 'Argument found';
+if (process.argv.length === 2) message = 'No argument';
+else if (process.argv.length === 3) message = 'Argument found';
 console.log(message);
